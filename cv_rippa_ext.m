0e61fab97bdb26572162ccd29d2cbe4236181d70
Francesco Marchetti <47693812+cesc14@users.noreply.github.com>
2021-02-03 13:17:53 +0100

    Add files via upload

---
+++ cv_rippa_ext.m
@@ -0,0 +1,75 @@
+function [ep_best,val,timez] = cv_rippa_ext(dsites,rhs,rbf,ep,n_folds,...
+    the_norm)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% The extension of the Rippa's method to k-fold CV for the tuning of the
+% shape parameter.
+% This function is inspired by LOOCV2D.m by G. Fasshauer.
+% Calls on: DistanceMatrix.m by G. Fasshauer
+%
+% Input
+%   dsites: Mxs matrix representing a set of M data sites in R^s
+%              (i.e., each row contains one s-dimensional point)
+%   rhs: Mx1 matrix of function values at the dsites
+%   rbf: function handle, the chosen RBF for the interpolation
+%   ep: Dx1 matrix of values for the tuning of the shape parameter
+%   n_folds: number of folds k for the k-fold CV
+%   the_norm: chosen norm for the validation error
+%
+% Output
+%   ep_best: the optimal value of the shape parameter
+%   val: the validation error
+%   timez: the time employed by the k-fold CV process
+%
+% To use this function, please cite:
+% F. Marchetti, "The extension of the Rippa's algorithm beyond LOOCV",
+% submitted.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+rng(42);
+folds = cvpartition(length(rhs),'KFold',n_folds);
+
+DM_data = DistanceMatrix(dsites,dsites);
+maxEF = zeros(length(ep),1);
+
+tic
+
+for i=1:length(ep)
+    
+    EF = zeros(length(rhs),1);
+    IM = rbf(ep(i),DM_data);
+
+    invIM = inv(IM);
+    coeffs = invIM*rhs;
+    
+    if n_folds == 1
+        
+        % The standard Rippa's scheme
+        
+        EF = invIM*(rhs)./diag(invIM);
+       
+    else
+        
+        pos = 1;
+        
+        for j=1:n_folds
+            
+            test_ind = test(folds,j);
+            invIM_loc = invIM(test_ind,test_ind);   
+
+            EF(pos:pos+sum(test_ind)-1) = invIM_loc\coeffs(test_ind);
+            pos = pos + sum(test_ind);
+        end        
+                
+    end
+    
+    maxEF(i) = norm(EF(:),the_norm);
+
+
+end
+
+ep_best = ep(maxEF==min(maxEF));
+val = min(maxEF);
+timez = toc;
---
+++ demo.m
@@ -0,0 +1,48 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Script that perform the parameter tuning in RBF interpolation using the
+% extended Rippa's scheme. To use this script, please cite:
+% F. Marchetti, "The extension of the Rippa's algorithm beyond LOOCV",
+% submitted.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+clear all
+
+% The interpolation dataset
+
+[xx,yy] = meshgrid(linspace(-1,1,30));
+dsites = [xx(:) yy(:)];
+
+% The vector for the shape parameter tuning
+mine = 0.01; maxe = 0.5; ne = 101;
+ep = linspace(mine,maxe,ne);
+
+% The chosen norm
+
+the_norm = inf;
+
+% The number k of k-fold CV
+
+n_folds = 10;
+
+% The test function and function values at the data sites
+
+f = @(x,y) sin(x)./(x.^2+1) .* cos(y)./(y.^2+1);
+rhs = f(dsites(:,1),dsites(:,2));  
+
+% The chosen RBF
+
+rbf = @(e,r) max(0,1-e.*r).^4.*(4.*(e.*r) + 1); %Wendland c2
+
+% The extended Rippa's algorithm
+
+[ep_best,val,timez]=cv_rippa_ext(dsites,rhs,rbf,ep,n_folds,the_norm);
+    
+
+fprintf('Best parameter value: %f\n' , ep_best)
+fprintf('Validation error: %f\n' , ep_best)
+fprintf('Time employed: %f\n' , timez)
+
+    
